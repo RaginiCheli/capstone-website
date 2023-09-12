941fffd48fa1d93b30e49002310a1b2ac02bbd38
RaginiCheli <144846496+RaginiCheli@users.noreply.github.com>
2023-09-12 16:47:26 -0500

    changes made for home, mentor, team page
    added pages Home, Mentor, Team details

--- src/Team.js
+++ src/Team.js
@@ -1,10 +1,43 @@
 import * as React from 'react';
+import Team1 from "./mentor.jpg";
+import Team2 from "./mentor.jpg";
+import Team3 from "./mentor.jpg";
+import Team4 from "./mentor.jpg";
 
 function Team() {
     return (
-        <div>
-<h1>Teams tab</h1>
+      <div>
+      <div class="container"> 
+        <div class="image">
+           <img src={Team1} className="app-logo"alt="logo"></img>
+           <h1>Name</h1>
+           <h1>Computer Science</h1>
+           <h1>Linkedin</h1>
+           <h1>Values</h1>
         </div>
+        <div class="image">
+           <img src={Team2} className="app-logo"alt="logo"></img>
+           <h1>Name</h1>
+           <h1>Computer Science</h1>
+           <h1>Linkedin</h1>
+           <h1>Values</h1>
+        </div>
+        <div class="image">
+           <img src={Team3} className="app-logo"alt="logo"></img>
+           <h1>Name</h1>
+           <h1>Computer Science</h1>
+           <h1>Linkedin</h1>
+           <h1>Values</h1>
+        </div>
+        <div class="image">
+           <img src={Team4} className="app-logo"alt="logo"></img>
+           <h1>Name</h1>
+           <h1>Computer Science</h1>
+           <h1>Linkedin</h1>
+           <h1>Values</h1>
+        </div>  
+      </div>
+  </div>
       );
 
 }
